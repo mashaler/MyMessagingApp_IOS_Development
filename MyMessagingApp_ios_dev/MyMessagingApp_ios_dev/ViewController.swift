551346ad787d7052932f43890f5b2d480cac0bab
Rolva Mashale <mashalerolva@gmail.com>
2023-06-19 09:12:22 +0200

    First Commit

---
+++ MyMessagingApp_ios_dev/MyMessagingApp_ios_dev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMessagingApp_ios_dev
+//
+//  Created by DA MAC  M1 154 on 2023/06/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
